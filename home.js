2a03b3411be2150073f81bd867c913b41a7a5472
Md Hasibur Rahman <hasib@MddeMacBook-Pro.local>
2025-08-25 02:48:00 +0800

    feature added Transfer money and get bonus

--- home.js
+++ home.js
@@ -1,3 +1,23 @@
+function getInputValueNumber(id) {
+  const inputField = document.getElementById(id);
+  const inputFieldValue = inputField.value;
+  const inputFieldValueNumber = parseInt(inputFieldValue);
+  return inputFieldValueNumber;
+}
+
+function getInnerTextNumber(id) {
+  const element = document.getElementById(id);
+  const innerText = element.innerText;
+  const innerTextNumber = parseInt(innerText);
+  return innerTextNumber;
+}
+
+function setInnerText(value) {
+  const availableBalanceElement = document.getElementById("available-balance");
+  availableBalanceElement.innerText = value;
+}
+
+// add money feature
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
@@ -5,12 +25,10 @@ document
 
     const bankName = document.getElementById("add-bank").value;
     const accountNumber = document.getElementById("add-account").value;
-    const addAmount = parseInt(document.getElementById("add-amount").value);
-    const addPin = parseInt(document.getElementById("add-pin").value);
+    const addAmount = getInputValueNumber("add-amount");
+    const addPin = getInputValueNumber("add-pin");
     const pin = 1234;
-    const availableBalance = parseInt(
-      document.getElementById("available-balance").innerText
-    );
+    const availableBalance = getInnerTextNumber("available-balance");
 
     if (accountNumber.length < 11) {
       alert("please provide valid account number");
@@ -23,7 +41,7 @@ document
 
     const newBalance = availableBalance + addAmount;
 
-    document.getElementById("available-balance").innerText = newBalance;
+    setInnerText(newBalance);
 
     document.getElementById("add-bank").value = "";
     document.getElementById("add-account").value = "";
@@ -36,14 +54,10 @@ document
 document.getElementById("withdraw-btn").addEventListener("click", function (e) {
   e.preventDefault();
   const agentNumber = document.getElementById("agent-number").value;
-  const withdrawAmount = parseInt(
-    document.getElementById("withdraw-amount").value
-  );
-  const withdrawPin = parseInt(document.getElementById("withdraw-pin").value);
+  const withdrawAmount = getInputValueNumber("withdraw-amount");
+  const withdrawPin = getInputValueNumber("withdraw-pin");
   const pin = 1234;
-  const availableBalance = parseInt(
-    document.getElementById("available-balance").innerText
-  );
+  const availableBalance = getInnerTextNumber("available-balance");
 
   if (agentNumber.length < 11) {
     alert("please provide valid account number");
@@ -56,21 +70,88 @@ document.getElementById("withdraw-btn").addEventListener("click", function (e) {
 
   const newBalance = availableBalance - withdrawAmount;
 
-  document.getElementById("available-balance").innerText = newBalance;
+  setInnerText(newBalance);
 
   document.getElementById("agent-number").value = "";
   document.getElementById("withdraw-amount").value = "";
   document.getElementById("withdraw-pin").value = "";
 });
 
+// transfer money feature
+
+document.getElementById("send-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const userAccountNumber = document.getElementById("user-number").value;
+  const sendAmount = getInputValueNumber("send-amount");
+  const transferPin = getInputValueNumber("transfer-pin");
+  const pin = 1234;
+  const availableBalance = getInnerTextNumber("available-balance");
+
+  if (userAccountNumber.length < 11) {
+    alert("please provide valid account number");
+    return;
+  }
+  if (pin !== transferPin) {
+    alert("please provide valid pin number");
+    return;
+  }
+
+  const newBalance = availableBalance - sendAmount;
+
+  setInnerText(newBalance);
+
+  document.getElementById("user-number").value = "";
+  document.getElementById("send-amount").value = "";
+  document.getElementById("transfer-pin").value = "";
+});
+
+// Get Bonus feature
+
+document.getElementById("bonus-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const bonusCoupon1 = 1005;
+  const bonusCoupon2 = 1010;
+  const getBonusInputValue = getInputValueNumber("bonus-coupon");
+  const availableBalance = getInnerTextNumber("available-balance");
+
+  if (getBonusInputValue === bonusCoupon1) {
+    setInnerText(availableBalance + 5);
+  } else if (getBonusInputValue === bonusCoupon2) {
+    setInnerText(availableBalance + 10);
+  } else {
+    alert("please provide valid coupon number");
+  }
+
+  document.getElementById("bonus-coupon").value = "";
+});
+
 //   toggling feature
 
 document.getElementById("add-btn").addEventListener("click", function () {
   document.getElementById("cashout-parent").style.display = "none";
+  document.getElementById("transfer-parent").style.display = "none";
+  document.getElementById("bonus-parent").style.display = "none";
   document.getElementById("add-money-parent").style.display = "block";
 });
 
 document.getElementById("cashout-btn").addEventListener("click", function () {
   document.getElementById("add-money-parent").style.display = "none";
+  document.getElementById("transfer-parent").style.display = "none";
+  document.getElementById("bonus-parent").style.display = "none";
   document.getElementById("cashout-parent").style.display = "block";
+});
+
+document.getElementById("transfer-btn").addEventListener("click", function () {
+  document.getElementById("add-money-parent").style.display = "none";
+  document.getElementById("cashout-parent").style.display = "none";
+  document.getElementById("bonus-parent").style.display = "none";
+  document.getElementById("transfer-parent").style.display = "block";
+});
+
+document.getElementById("btn-bonus").addEventListener("click", function () {
+  document.getElementById("add-money-parent").style.display = "none";
+  document.getElementById("cashout-parent").style.display = "none";
+  document.getElementById("transfer-parent").style.display = "none";
+  document.getElementById("bonus-parent").style.display = "block";
 });
