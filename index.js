4dd6b3d4bbb231adf6271c70e9efc92671570138
Md Hasibur Rahman <hasib@MddeMacBook-Pro.local>
2025-08-24 13:05:50 +0800

    login page

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("btn-login").addEventListener("click", function (e) {
+  e.preventDefault();
+  const MobileNumber = 13032875673;
+  const pin = 1234;
+
+  const mobileNumberValue = parseInt(
+    document.getElementById("mobile-number").value
+  );
+
+  const pinNumberValue = parseInt(document.getElementById("input-pin").value);
+
+  if (MobileNumber === mobileNumberValue && pin === pinNumberValue) {
+    window.location.href = "./home.html";
+  } else {
+    alert("invalid credential");
+  }
+});
